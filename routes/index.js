830298e42cdb8ea686d8008aa10ac80d81ebafc6
gavin <gx199492@qq.com>
2017-09-19 14:00:40 +0800

    不同步数据库文件

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,9 @@ let router = express.Router()
 let path = require('path')
 
 /* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render(path.join(__dirname, '../views/index.jade'), { title: 'Express' });
-// });
+router.get('/', function(req, res, next) {
+  res.render(path.join(__dirname, '../views/index.jade'), { title: 'Express' });
+});
 
 router.get('/task', function (req, res) {
   res.sendFile(path.join(__dirname, '../frontend/dist/index.html'))
