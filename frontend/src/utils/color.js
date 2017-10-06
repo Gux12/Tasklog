80529429bb29bfbb19f12c5173e17a970f357df9
gavin <gx199492@qq.com>
2017-10-07 01:19:29 +0800

    解决crypto未找到的bug

--- frontend/src/utils/color.js
+++ frontend/src/utils/color.js
@@ -1,6 +1,10 @@
 export async function getRandomColor (str) {
   let buffer = new TextEncoder('utf-8').encode(str)
-  let hash = await crypto.subtle.digest('SHA-256', buffer)
-  return '#' + new DataView(hash).getUint32(0).toString(16).slice(0, 6)
-  // return '#' + ('00000' + ((Math.random() * 16777215 + 0.5) >> 0).toString(16)).slice(-6)
+  let crypto = window.crypto || window.msCrypto
+  if (crypto) {
+    let hash = await crypto.subtle.digest('SHA-256', buffer)
+    return '#' + new DataView(hash).getUint32(0).toString(16).slice(0, 6)
+  } else {
+    return '#000000'
+  }
 }
